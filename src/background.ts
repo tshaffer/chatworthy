2c5fb0ca12cf3adb5fc4a74100c9e7da718aa381
Ted Shaffer <shaffer.family@gmail.com>
2025-10-26 05:25:08 -0700

    removed obsolete code

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,2 @@
 
 console.log('[Chatworthy] background worker boot');
-chrome.commands.onCommand.addListener(async (command) => {
-  if (command === 'export-chat') {
-    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    if (!tab?.id) return;
-    chrome.tabs.sendMessage(tab.id, { type: 'CHATWORTHY_EXPORT', format: 'md' });
-  }
-});
